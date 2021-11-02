23886f44d29f9c3e1c0d8820a812068fe85988ff
alexgithui91 <alexgithui91@gmail.com>
2021-11-02 15:42:47 +0300

    adjustments to emailer

--- read_email.py
+++ read_email.py
@@ -3,10 +3,10 @@ import smtplib
 smtpObj = smtplib.SMTP("smtp-mail.outlook.com", 587)
 smtpObj.ehlo()
 smtpObj.starttls()
-smtpObj.login("alex.githui@dayliff.com", "al3xg1thu1")
-# smtpObj.sendmail(
-#     "agithui@dayliff.com",
-#     "agithui@dayliff.com",
-#     "Subject: So long.\nDear Alex, so long and thanks for all the fish. Sincerely, Testing",
-# )
-# smtpObj.quit()
+smtpObj.login("alexgithui91@outlook.com", "al3xg1thu1")
+smtpObj.sendmail(
+    "alexgithui91@outlook.com",
+    "alexgithui91@gmail.com",
+    "Subject: So long.\nDear Alex, so long and thanks for all the fish. Sincerely, Testing",
+)
+smtpObj.quit()
