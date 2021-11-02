e5e64daf054f3c1f37c8b6621ccb12d8f2c3eb36
alexgithui91 <alexgithui91@gmail.com>
2021-11-02 16:47:52 +0300

    wip email reader

--- read_email.py
+++ read_email.py
@@ -1,12 +1,11 @@
-import smtplib
+import imapclient
 
-smtpObj = smtplib.SMTP("smtp-mail.outlook.com", 587)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login("alexgithui91@outlook.com", "al3xg1thu1")
-smtpObj.sendmail(
-    "alexgithui91@outlook.com",
-    "alexgithui91@gmail.com",
-    "Subject: So long.\nDear Alex, so long and thanks for all the fish. Sincerely, Testing",
-)
-smtpObj.quit()
+imapObj = imapclient.IMAPClient("imap-mail.outlook.com", ssl=True)
+
+imapObj.login("agithui@dayliff.com", "al3xg1thu1")
+
+imapObj.select_folder("INBOX", readonly=True)
+
+# UIDs = imapObj.search(["UNSEEN"])
+
+# print(UIDs)
