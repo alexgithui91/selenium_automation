5d946be1227def2e6209c78c2bce2215c0126e4a
alexgithui91 <alexgithui91@gmail.com>
2021-11-04 15:36:28 +0300

    working upload to db

--- upload_ticket.py
+++ upload_ticket.py
@@ -1,1 +1,10 @@
 import pyodbc
+import pandas as pd
+
+conn = pyodbc.connect(
+    "Driver={SQL Server Native Client 11.0};"
+    "Server=.;"
+    "Database=TestDB;"
+    "Trusted_Connection=yes;"
+)
+cursor = conn.cursor()
