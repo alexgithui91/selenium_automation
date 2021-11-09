6180a48fa4917f5fbd8ec6a18abe558913c43a82
alexgithui91 <alexgithui91@gmail.com>
2021-11-09 12:27:43 +0300

    cleaned imports retrieve_tickets.py

--- retrieve_tickets.py
+++ retrieve_tickets.py
@@ -5,7 +5,6 @@ import webbrowser
 from datetime import datetime
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from webdriver_manager import firefox
