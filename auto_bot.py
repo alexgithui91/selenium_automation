55721f1eb2e9eaeef2850a0f4939836a8f4a7d75
alexgithui91 <alexgithui91@gmail.com>
2022-02-11 15:06:50 +0300

    bot able to login to Hackerrank.com

--- auto_bot.py
+++ auto_bot.py
@@ -1,22 +1,33 @@
+import time
+import os
 from dotenv import load_dotenv
 from selenium import webdriver
 
-# from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.common.keys import Keys
-
-
-# print(driver.title)
-# search_bar = driver.find_element_by_name("q")
-# search_bar.clear()
-# search_bar.send_keys("getting started with python")
-# search_bar.send_keys(Keys.RETURN)
-# print(driver.current_url)
-
 
 def run_bot():
+    # Get Email user name and password
+    load_dotenv()
+    user_name = os.environ.get("user_email")
+    user_pwd = os.environ.get("user_pwd")
+
     driver = webdriver.Chrome("./chromedriver")
-    driver.get("https://gmail.com")
-    # driver.close()
+    driver.maximize_window()
+    driver.get("https://www.hackerrank.com/auth/login")
+
+    email_input = driver.find_element_by_id("input-1")
+    email_input.send_keys(user_name)
+
+    password_input = driver.find_element_by_id("input-2")
+    password_input.send_keys(user_pwd)
+
+    log_in = driver.find_element_by_xpath(
+        "//*[@id='tab-1-content-1']/div[1]/form/div[4]/button"
+    )
+    log_in.click()
+
+    time.sleep(5)
+
+    driver.close()
 
 
 if __name__ == "__main__":
